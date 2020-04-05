174b62699830e5ed906cd94cc4d7a6658ca97a8d
prithvitharun <prithvitharun@prithvis-air>
2020-04-05 16:50:18 +0530

    Completed frontend development using angular and fetching data with backend.

---
+++ Angular-OnlineBookStore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../../common-class/book';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  books: Book[] ;
+
+  constructor(private _bookService:BookService) { }
+
+  ngOnInit() {
+
+    this._bookService.getBooks().subscribe(data=>{this.books=data});
+  }
+
+
+}
